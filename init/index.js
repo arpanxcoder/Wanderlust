d584ea075616b33b8da0c954060bf98f5639c132
Rupesh Shinde <arpanshinde193@gmail.com>
2026-02-13 21:01:42 +0530

    Connected MongoDB Atlas and initialized data

--- init/index.js
+++ init/index.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const mongoose  = require("mongoose");
 const initData = require("./data.js");
 
 const Listing = require("../models/listing.js");
 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
+
  main()
     .then(()=>{
      console.log("connected to the DB");
@@ -13,7 +15,7 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 const initDB = async  () =>{
      await Listing.deleteMany({});
